6b4d7ff2fee0f396fd551aa91f86b7f18e9ece50
Evgeniy Serookiy <serookiy.evgeniy@gmail.com>
2025-02-09 18:04:33 +0300

    Commit

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Migrations/20250209143023_Update14.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update14 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "apartment",
+                table: "Pets",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "building",
+                table: "Pets",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "apartment",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "building",
+                table: "Pets");
+        }
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Migrations/20250209150010_Update21.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update21 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "Species",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "Breeds",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "Species");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "Breeds");
+        }
+    }
+}
